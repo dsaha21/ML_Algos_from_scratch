82fd118c8a0d0dac99645e13f19759975a4244b8
Dripto Saha <sahadripto21@gmail.com>
2024-11-04 22:44:08 +0530

    add: vector_machines

---
+++ ml_scratch/classifications.py
@@ -0,0 +1,13 @@
+import numpy as n
+
+class SVM:
+    def __init__(self, learning_rate, n_iters):
+        self.w = None
+        self.b = None
+        self.iter = n_iters
+        self.lr = learning_rate
+    
+    def fit(self, X, y):
+        pass
+    def predict(self, X):
+        pass
